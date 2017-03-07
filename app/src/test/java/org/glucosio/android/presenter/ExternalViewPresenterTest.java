c113a67878ed7f7022c7c9361341e52fb0926c2b
Eugen Martynov <eugenemartynov@gmail.com>
2017-03-07 07:34:22 +0100

    Whitespaces refactoring

--- app/src/test/java/org/glucosio/android/presenter/ExternalViewPresenterTest.java
+++ app/src/test/java/org/glucosio/android/presenter/ExternalViewPresenterTest.java
@@ -1,13 +1,10 @@
 package org.glucosio.android.presenter;
 
-import org.glucosio.android.BuildConfig;
 import org.glucosio.android.RobolectricTest;
 import org.glucosio.android.tools.network.GlucosioExternalLinks;
 import org.glucosio.android.tools.network.NetworkConnectivity;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.annotation.Config;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -15,40 +12,42 @@ import static org.mockito.Mockito.when;
 
 public class ExternalViewPresenterTest extends RobolectricTest {
 
-  private ExternalViewPresenter.View view;
-  private ExternalViewPresenter presenter;
-  private NetworkConnectivity network;
+    private ExternalViewPresenter.View view;
+    private ExternalViewPresenter presenter;
+    private NetworkConnectivity network;
 
-  @Before public void setUp() throws Exception {
-    view = mock(ExternalViewPresenter.View.class);
-    network = mock(NetworkConnectivity.class);
-    presenter = new ExternalViewPresenter(view, network);
-  }
+    @Before
+    public void setUp() throws Exception {
+        view = mock(ExternalViewPresenter.View.class);
+        network = mock(NetworkConnectivity.class);
+        presenter = new ExternalViewPresenter(view, network);
+    }
 
-  @Test(expected = IllegalArgumentException.class)
-  public void shouldThrowException_WhenNoParameters() throws Exception {
-    when(network.isConnected()).thenReturn(true);
-    when(view.extractTitle()).thenReturn(null);
-    when(view.extractUrl()).thenReturn(null);
-    presenter.onViewCreated();
-  }
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowException_WhenNoParameters() throws Exception {
+        when(network.isConnected()).thenReturn(true);
+        when(view.extractTitle()).thenReturn(null);
+        when(view.extractUrl()).thenReturn(null);
+        presenter.onViewCreated();
+    }
 
-  @Test public void shouldLoadOpenSourceLicenses_WhenLicenseParameters() throws Exception {
-    String LICENSES = "licenses";
-    when(view.extractUrl()).thenReturn(GlucosioExternalLinks.LICENSES);
-    when(view.extractTitle()).thenReturn(LICENSES);
-    when(network.isConnected()).thenReturn(true);
+    @Test
+    public void shouldLoadOpenSourceLicenses_WhenLicenseParameters() throws Exception {
+        String LICENSES = "licenses";
+        when(view.extractUrl()).thenReturn(GlucosioExternalLinks.LICENSES);
+        when(view.extractTitle()).thenReturn(LICENSES);
+        when(network.isConnected()).thenReturn(true);
 
-    presenter.onViewCreated();
+        presenter.onViewCreated();
 
-    verify(view).loadExternalUrl(GlucosioExternalLinks.LICENSES);
-    verify(view).setupToolbarTitle(LICENSES);
-  }
+        verify(view).loadExternalUrl(GlucosioExternalLinks.LICENSES);
+        verify(view).setupToolbarTitle(LICENSES);
+    }
 
-  @Test
-  public void shouldInvokeShowNoConnectionWarning_WhenNetworkIsNotConnected() throws Exception {
-    when(network.isConnected()).thenReturn(false);
-    presenter.onViewCreated();
-    verify(view).showNoConnectionWarning();
-  }
+    @Test
+    public void shouldInvokeShowNoConnectionWarning_WhenNetworkIsNotConnected() throws Exception {
+        when(network.isConnected()).thenReturn(false);
+        presenter.onViewCreated();
+        verify(view).showNoConnectionWarning();
+    }
 }
